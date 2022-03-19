027c52254af1e600a410f635c1283144ffa0b35f
Lmaxplay <lmaxplay@outlook.com>
2022-03-19 13:22:43 +0100

    Minor patch

--- Scripts/data.ps1
+++ Scripts/data.ps1
@@ -1,1 +1,4 @@
-$Tips = 'You can use Update-PowerShell-Additions to update!', 'Powershell Additions was made since PowerShell lacked colors by default!', 'Use Write-Host instead of Write-Output or echo to get more control and use colors'
+$Tips = @('You can use the cmdlet Update-PowerShell-Additions to update!',
+'Powershell Additions was made since PowerShell lacked colors by default!',
+'Use Write-Host instead of Write-Output or echo to get more control and use colors!'
+)
