eb7af7e76ac6e9025f9a8c764733fbd53e5e469f
Lmaxplay <lmaxplay@outlook.com>
2022-03-23 16:35:45 +0100

    tiny fixes

--- profile.ps1
+++ profile.ps1
@@ -19,7 +19,7 @@ Write-Host -NoNewline "PowerShell Additions" -ForegroundColor:Blue
 if($PowerShellAdditionsVersion -ne "") {
     Write-Host -NoNewline " "
 }
-Write-Host -NoNewline "$PowerShellAdditionsVersion " -ForegroundColor:Blue
+Write-Host -NoNewline "$fVersion " -ForegroundColor:Blue
 Write-Host -NoNewline "($PowerShellAdditionsCodename)" -ForegroundColor:Blue
 Write-Host -NoNewline " for PowerShell 7.2.2`n" -ForegroundColor:DarkGray
 
