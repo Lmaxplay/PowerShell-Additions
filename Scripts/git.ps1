9f9c27d2b6442719db8b92fca59c3b91273a0002
Lmaxplay <lmaxplay@outlook.com>
2022-03-23 16:36:02 +0100

    added a single space, thats it

--- Scripts/git.ps1
+++ Scripts/git.ps1
@@ -1,5 +1,5 @@
 function Get-GitBranch {
-    if(Test-Path ((Get-Location).Path + "./.git/")) {
+    if (Test-Path ((Get-Location).Path + "./.git/")) {
         return (git rev-parse --abbrev-ref HEAD) 2> $null
     } else {
         return $null;
