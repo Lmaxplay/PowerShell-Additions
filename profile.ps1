ba34c9399c2739634b887dab1f18d6eb396cfc1a
Lmaxplay <lmaxplay@outlook.com>
2022-03-30 15:53:13 +0200

    tiny update for double space fix

--- profile.ps1
+++ profile.ps1
@@ -23,7 +23,7 @@ Write-Host -NoNewline "PowerShell Additions" -ForegroundColor:Blue
 if($PowerShellAdditionsVersion -ne "") {
     Write-Host -NoNewline " "
 }
-Write-Host -NoNewline "$PowerShellAdditionsVersion " -ForegroundColor:Blue
+Write-Host -NoNewline "$PowerShellAdditionsVersion" -ForegroundColor:Blue
 #Write-Host -NoNewline "($PowerShellAdditionsCodename)" -ForegroundColor:Blue
 Write-Host -NoNewline " for PowerShell 7.2.2`n" -ForegroundColor:DarkGray
 
