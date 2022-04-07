744d3b9e603d1ae4a7082516de588f4a089c5a18
Lmaxplay <lmaxplay@outlook.com>
2022-04-07 20:59:53 +0200

    Git optimalizations and git theme And THEME_DEFAULT_GIT is now used when git is installed and on path

--- Scripts/git.ps1
+++ Scripts/git.ps1
@@ -1,7 +1,25 @@
 function Get-GitBranch {
-    if (Test-Path ((Get-Location).Path + "./.git/")) {
-        return (git rev-parse --abbrev-ref HEAD) 2> $null
+    $output = Get-Location | ForEach-Object {
+        $path = $_.Path
+        do {
+            # Write-Host $path
+            $path
+            $path = Split-Path -Path $path -Parent        
+        } while ($path)
+    } |% {
+        $headPath = "$_/.git/HEAD"
+        if (Test-Path $headPath) {
+            $ref = get-content $headPath -TotalCount 1        
+            if ($ref.StartsWith("ref:")) {
+                $ref.Substring(5).Split("/") | select -Skip 2 | Join-String -Separator "/"
+            } else {
+                $ref.Substring(0, 8)
+            }
+        }
+    } | Select -First 1
+    if($output -eq "") {
+        return $null
     } else {
-        return $null;
+        return $output
     }
 }
--- profile.ps1
+++ profile.ps1
@@ -2,11 +2,18 @@
 
 . (Join-Path $PsScriptRoot "Scripts/index.ps1")
 
-Set-Variable -Name "PowerShellAdditionsVersion" -Value (New-Object -TypeName Version -ArgumentList @(1, 5, 1)) -Option Constant -Scope global
+Set-Variable -Name "PowerShellAdditionsVersion" -Value (New-Object -TypeName Version -ArgumentList @(1, 5, 2)) -Option Constant -Scope global
 Set-Variable -Name "PowerShellAdditionsPowerShellSupportedVersion" -Value (New-Object -TypeName Version -ArgumentList @(7, 2, 2)) -Option Constant -Scope global
 #Set-Variable -Name "PowerShellAdditionsCodename" -Value "" -Option Constant -Scope global
 
 $THEME = "THEME_DEFAULT"
+
+try {
+    git | Out-Null
+    $THEME = "THEME_DEFAULT_GIT"
+} catch {
+    $THEME = "THEME_DEFAULT"
+}
 
 if ((Test-Path (Join-Path $PsScriptRoot "preload.ps1"))) {
     . (Join-Path $PsScriptRoot "preload.ps1");
